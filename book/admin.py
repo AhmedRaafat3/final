ecf5cceb9388e91268ff52dffeba9ac866a0dde5
AhmedRaafat3 <ahmedmostafarafat22@gmail.com>
2023-11-27 01:27:47 +0200

    create models

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author,Book,Review
+
+
+admin.site.register(Author)
+
+admin.site.register(Book)
+
+admin.site.register(Review)
 
 # Register your models here.
--- book/models.py
+++ book/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    biography = models.TextField(max_length=1000)
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    publication_data = models.DateField(default=timezone.now)
+    price = models.IntegerField()
+
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=30)
+    content = models.TextField(max_length=500)
+   
+
+   
